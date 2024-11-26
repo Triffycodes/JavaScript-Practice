abb94179d10c5428d6001b3f463c027f1d39de3a
Adarsh Shankar <45749291+Triffycodes@users.noreply.github.com>
2024-11-26 03:50:39 +0000

    new keyword importance

---
+++ 02_practice.js
@@ -0,0 +1,14 @@
+function User(username,email){
+    this.username = username
+    this.email = email
+    //console.log(username,email);
+    return this
+}
+
+//let userOne = new User("Adarsh","Adarsh@google.com")
+let userOne = new User("Histehs","histes@google.com")
+let userTwo = new User("Adarsh","Adarsh@google.com")
+
+console.log(userTwo);
+
+
